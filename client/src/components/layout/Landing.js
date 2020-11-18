17d21ec2c5cd71ff682ee696ec84b2ccc45a6106
Zach Goldberg <zach.goldberg@inventive.io>
2020-11-18 02:30:59 -0600

    styles

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,9 +1,24 @@
+import '../stylesheets/Landing.css';
+
 const Landing = () => {
     return (
-        <div className="container">
-            <div className="row align-items-center justify-content-center">
-                <h1>Landing</h1>
+        <div className="landing-comp d-flex align-items-center">
+            <div className="container">
+                <div className="row align-items-center justify-content-center">
+                    <div className="action-container d-flex flex-column justify-content-center align-items-center">
+                        <form>
+                            <label>
+                                <span>Game Code:</span>
+                                <br/>
+                                <input type="text" name="name" />
+                            </label>
+                            <br/>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                </div>
             </div>
+            <div className="manager">click here to manage</div>
         </div>
     )
 }
