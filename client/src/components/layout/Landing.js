81bb311aa510c90787c80bc5e75a6f88964af8c5
Zach Goldberg <zach.goldberg@inventive.io>
2020-11-18 04:05:48 -0600

    cool animation

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,17 +5,20 @@ const Landing = () => {
         <div className="landing-comp d-flex align-items-center">
             <div className="container">
                 <div className="row align-items-center justify-content-center">
-                    <div className="action-container d-flex flex-column justify-content-center align-items-center">
-                        <form>
+                    <div className="action-container">
+                        <div className="zaction-container d-flex align-items-center justify-content-center">
+                            <form>
                             <label>
                                 <span>Game Code:</span>
-                                <br/>
+                                <br />
                                 <input type="text" name="name" />
                             </label>
-                            <br/>
+                            <br />
                             <input type="submit" value="Submit" />
                         </form>
+                        </div>
                     </div>
+                    <div className="magic-container"></div>
                 </div>
             </div>
             <div className="manager">click here to manage</div>
