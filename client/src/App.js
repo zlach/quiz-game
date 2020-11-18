0144c82958f2ee28c94f29f76b6d21969ab35dc2
Zach Goldberg <zach.goldberg@inventive.io>
2020-11-18 00:52:15 -0600

    basic client setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Landing from './components/layout/Landing';
+
+const App = () =>
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={Landing}/>
+    </Fragment>
+  </Router>
+
+
+export default App;
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+const Landing = () => {
+    return (
+        <div className="container">
+            <div className="row align-items-center justify-content-center">
+                <h1>Landing</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
