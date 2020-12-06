f00453032fd7fc68afe03850246b3d6f22930800
Zach Goldberg <zach.goldberg@inventive.io>
2020-12-05 19:19:17 -0600

    more ui

--- client/src/components/forms/steps/PartsStep.js
+++ client/src/components/forms/steps/PartsStep.js
@@ -4,10 +4,16 @@ import { FormGroup, Input } from 'reactstrap';
 const PartsStep = (props) => {
 
     const Round = ({ round, index }) => (
-        <div className="border p-2 m-2">
-            <div>Round {index + 1}</div>
+        <div className="border rounded p-2 my-4 mx-5">
+            <h4>Round {index + 1}</h4>
             {round.map((question, i) => {
-                return <span>{question.parts}</span>
+                return (
+                    <div className="d-inline-block rounded border p-2 m-2">
+                        <h6>Question #{i + 1}</h6>
+                        <div>Parts: {question.parts}</div>
+                        <div>Points (per part): {question.pointsPerPart}</div>
+                    </div>
+                )
             })}
         </div>
     );
